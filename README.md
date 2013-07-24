bdbe367482c5c72c3f66e5e0bffedf9dd4ffec30
amazingsurge <amazingsurge@yahoo.com>
2013-07-24 18:53:07 +0800

    update package.json

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ Sticker is jQuery plugin used to stick elements to the fixed position on your we
 ## Getting Started
 Download the [production version][min] or the [development version][max].
 
-[min]: https://raw.github.com/amazingSurge/sticker/master/dist/jquery.sticker.min.js
-[max]: https://raw.github.com/amazingSurge/sticker/master/dist/jquery.sticker.js
+[min]: https://raw.github.com/amazingSurge/jquery-sticker/master/dist/jquery-sticker.min.js
+[max]: https://raw.github.com/amazingSurge/jquery-sticker/master/dist/jquery-sticker.js
 
 In your web page:
 
 ```html
 <script src="jquery.js"></script>
-<script src="dist/jquery.sticker.min.js"></script>
+<script src="dist/jquery-sticker.min.js"></script>
 <script>
 jQuery(function($) {
 	$('#header').sticker({
--- demo/js/main.js
+++ demo/js/main.js
@@ -1,38 +1,38 @@
 jQuery(document).ready(function() {
-    $('header').sticker({
-        topSpace: 0
-    });
+	$('header').sticker({
+		topSpace: 0
+	});
 
-    $('h1').sticker({
-        topSpace: 20
-    });
+	$('h1').sticker({
+		topSpace: 20
+	});
 
-    $('footer').sticker({
-        type: 'fill',
-        bottomSpace: 0
-    });
+	$('footer').sticker({
+		type: 'fill',
+		bottomSpace: 0
+	});
 
-    $('.sidebar').sticker({
-        type: 'sidebar',
-        topSpace: 30
-    });
-    setInterval(function() {
-        $('p').css('marginTop', '200px');
-    }, 5000);
+	$('.sidebar').sticker({
+		type: 'sidebar',
+		topSpace: 30
+	});
+	setInterval(function() {
+		$('p').css('marginTop', '200px');
+	}, 5000);
 });
 
 enquire.register("screen and (max-width:767px)", {
-    match: function() {
-        $('header').sticker('disable');
-        $('h1').sticker('disable');
-        $('footer').sticker('disable');
-        $('.sidebar').sticker('disable');
-    },
+	match: function() {
+		$('header').sticker('disable');
+		$('h1').sticker('disable');
+		$('footer').sticker('disable');
+		$('.sidebar').sticker('disable');
+	},
 
-    unmatch: function() {
-        $('header').sticker('enable');
-        $('h1').sticker('enable');
-        $('footer').sticker('enable');
-        $('.sidebar').sticker('enable');
-    }
+	unmatch: function() {
+		$('header').sticker('enable');
+		$('h1').sticker('enable');
+		$('footer').sticker('enable');
+		$('.sidebar').sticker('enable');
+	}
 }).listen();
